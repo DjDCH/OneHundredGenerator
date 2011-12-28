e0f7334b7ee70367cdf9285695eb1a6a772b6416
DjDCH <bukkit.coding@djdch.com>
2011-12-27 23:48:19 -0500

    Added BiomePlains class.

---
+++ src/com/djdch/bukkit/onehundredgenerator/mc100/BiomePlains.java
@@ -0,0 +1,11 @@
+package com.djdch.bukkit.onehundredgenerator.mc100;
+
+public class BiomePlains extends BiomeBase {
+    protected BiomePlains(int paramInt) {
+        super(paramInt);
+
+        this.B.z = -999;
+        this.B.A = 4;
+        this.B.B = 10;
+    }
+}
