99a4d61a4d7d60dcff520a663bdb18f257478e74
DjDCH <bukkit.coding@djdch.com>
2011-12-28 02:10:24 -0500

    Added WorldGenHugeMushroom class.

---
+++ src/com/djdch/bukkit/onehundredgenerator/mc100/WorldGenHugeMushroom.java
@@ -0,0 +1,206 @@
+package com.djdch.bukkit.onehundredgenerator.mc100;
+
+import java.util.List;
+import java.util.Random;
+
+import net.minecraft.server.Block;
+import net.minecraft.server.ItemStack;
+import net.minecraft.server.World;
+
+import org.bukkit.Bukkit;
+import org.bukkit.block.BlockState;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.event.world.StructureGrowEvent;
+import org.bukkit.material.MaterialData;
+import org.bukkit.plugin.PluginManager;
+
+public class WorldGenHugeMushroom extends WorldGenerator {
+    private int a = -1;
+
+    public WorldGenHugeMushroom(int i) {
+        this.a = i;
+    }
+
+    public WorldGenHugeMushroom() {
+    }
+
+    @Override
+    public boolean a(World world, Random random, int i, int j, int k) {
+        return grow(world, random, i, j, k, null, null, null);
+    }
+
+    public boolean grow(World world, Random random, int i, int j, int k, StructureGrowEvent event, ItemStack itemstack, CraftWorld bukkitWorld) {
+        int l = random.nextInt(2);
+
+        if (this.a >= 0) {
+            l = this.a;
+        }
+
+        int i1 = random.nextInt(3) + 4;
+        boolean flag = true;
+
+        if ((j >= 1) && (j + i1 + 1 <= world.height)) {
+            for (int j1 = j; j1 <= j + 1 + i1; j1++) {
+                byte b0 = 3;
+
+                if (j1 == j) {
+                    b0 = 0;
+                }
+
+                for (int k1 = i - b0; (k1 <= i + b0) && (flag); k1++) {
+                    for (int l1 = k - b0; (l1 <= k + b0) && (flag); l1++) {
+                        if ((j1 >= 0) && (j1 < world.height)) {
+                            int i2 = world.getTypeId(k1, j1, l1);
+                            if ((i2 != 0) && (i2 != Block.LEAVES.id))
+                                flag = false;
+                        } else {
+                            flag = false;
+                        }
+                    }
+                }
+            }
+
+            if (!flag) {
+                return false;
+            }
+            j1 = world.getTypeId(i, j - 1, k);
+            if ((j1 != Block.DIRT.id) && (j1 != Block.GRASS.id) && (j1 != Block.MYCEL.id))
+                return false;
+            if (!Block.BROWN_MUSHROOM.canPlace(world, i, j, k)) {
+                return false;
+            }
+
+            if (event == null) {
+                world.setRawTypeId(i, j - 1, k, Block.DIRT.id);
+            } else {
+                BlockState dirtState = bukkitWorld.getBlockAt(i, j - 1, k).getState();
+                dirtState.setTypeId(Block.DIRT.id);
+                event.getBlocks().add(dirtState);
+            }
+
+            int j2 = j + i1;
+
+            if (l == 1) {
+                j2 = j + i1 - 3;
+            }
+
+            for (int k1 = j2; k1 <= j + i1; k1++) {
+                int l1 = 1;
+                if (k1 < j + i1) {
+                    l1++;
+                }
+
+                if (l == 0) {
+                    l1 = 3;
+                }
+
+                for (int i2 = i - l1; i2 <= i + l1; i2++) {
+                    for (int k2 = k - l1; k2 <= k + l1; k2++) {
+                        int l2 = 5;
+
+                        if (i2 == i - l1) {
+                            l2--;
+                        }
+
+                        if (i2 == i + l1) {
+                            l2++;
+                        }
+
+                        if (k2 == k - l1) {
+                            l2 -= 3;
+                        }
+
+                        if (k2 == k + l1) {
+                            l2 += 3;
+                        }
+
+                        if ((l == 0) || (k1 < j + i1)) {
+                            if (((i2 == i - l1) || (i2 == i + l1)) && ((k2 == k - l1) || (k2 == k + l1))) {
+                                continue;
+                            }
+                            if ((i2 == i - (l1 - 1)) && (k2 == k - l1)) {
+                                l2 = 1;
+                            }
+
+                            if ((i2 == i - l1) && (k2 == k - (l1 - 1))) {
+                                l2 = 1;
+                            }
+
+                            if ((i2 == i + (l1 - 1)) && (k2 == k - l1)) {
+                                l2 = 3;
+                            }
+
+                            if ((i2 == i + l1) && (k2 == k - (l1 - 1))) {
+                                l2 = 3;
+                            }
+
+                            if ((i2 == i - (l1 - 1)) && (k2 == k + l1)) {
+                                l2 = 7;
+                            }
+
+                            if ((i2 == i - l1) && (k2 == k + (l1 - 1))) {
+                                l2 = 7;
+                            }
+
+                            if ((i2 == i + (l1 - 1)) && (k2 == k + l1)) {
+                                l2 = 9;
+                            }
+
+                            if ((i2 == i + l1) && (k2 == k + (l1 - 1))) {
+                                l2 = 9;
+                            }
+                        }
+
+                        if ((l2 == 5) && (k1 < j + i1)) {
+                            l2 = 0;
+                        }
+
+                        if (((l2 == 0) && (j < j + i1 - 1)) || (Block.o[world.getTypeId(i2, k1, k2)] != 0))
+                            continue;
+                        if (event == null) {
+                            world.setRawTypeIdAndData(i2, k1, k2, Block.BIG_MUSHROOM_1.id + l, l2);
+                        } else {
+                            BlockState state = bukkitWorld.getBlockAt(i2, k1, k2).getState();
+                            state.setTypeId(Block.BIG_MUSHROOM_1.id + l);
+                            state.setData(new MaterialData(Block.BIG_MUSHROOM_1.id + l, (byte) l2));
+                            event.getBlocks().add(state);
+                        }
+                    }
+
+                }
+
+            }
+
+            for (k1 = 0; k1 < i1; k1++) {
+                int l1 = world.getTypeId(i, j + k1, k);
+                if (Block.o[l1] != 0)
+                    continue;
+                if (event == null) {
+                    world.setRawTypeIdAndData(i, j + k1, k, Block.BIG_MUSHROOM_1.id + l, 10);
+                } else {
+                    BlockState state = bukkitWorld.getBlockAt(i, j + k1, k).getState();
+                    state.setTypeId(Block.BIG_MUSHROOM_1.id + l);
+                    state.setData(new MaterialData(Block.BIG_MUSHROOM_1.id + l, 10));
+                    event.getBlocks().add(state);
+                }
+
+            }
+
+            if (event != null) {
+                Bukkit.getPluginManager().callEvent(event);
+                if (!event.isCancelled()) {
+                    for (BlockState state : event.getBlocks()) {
+                        state.update(true);
+                    }
+                    if ((event.isFromBonemeal()) && (itemstack != null)) {
+                        itemstack.count -= 1;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+}
