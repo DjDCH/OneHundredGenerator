f1589a7849f32dd4e5adfdc1297a611fa11ea681
DjDCH <bukkit.coding@djdch.com>
2011-12-28 19:00:08 -0500

    Fixed imports; Fixed local vars; Fixed conditions.

--- src/com/djdch/bukkit/onehundredgenerator/mc100/WorldGenLakes.java
+++ src/com/djdch/bukkit/onehundredgenerator/mc100/WorldGenLakes.java
@@ -3,6 +3,7 @@ package com.djdch.bukkit.onehundredgenerator.mc100;
 import java.util.Random;
 
 import net.minecraft.server.Block;
+import net.minecraft.server.EnumSkyBlock;
 import net.minecraft.server.Material;
 import net.minecraft.server.World;
 
@@ -25,7 +26,8 @@ public class WorldGenLakes extends WorldGenerator {
         boolean[] arrayOfBoolean = new boolean[2048];
 
         int i = paramRandom.nextInt(4) + 4;
-        for (int j = 0; j < i; j++) {
+        int j;
+        for (j = 0; j < i; j++) {
             double d1 = paramRandom.nextDouble() * 6.0D + 3.0D;
             double d2 = paramRandom.nextDouble() * 4.0D + 2.0D;
             double d3 = paramRandom.nextDouble() * 6.0D + 3.0D;
@@ -51,7 +53,7 @@ public class WorldGenLakes extends WorldGenerator {
         for (j = 0; j < 16; j++) {
             for (i1 = 0; i1 < 16; i1++) {
                 for (i2 = 0; i2 < 8; i2++) {
-                    int i3 = (arrayOfBoolean[((j * 16 + i1) * 8 + i2)] == 0) && (((j < 15) && (arrayOfBoolean[(((j + 1) * 16 + i1) * 8 + i2)] != 0)) || ((j > 0) && (arrayOfBoolean[(((j - 1) * 16 + i1) * 8 + i2)] != 0)) || ((i1 < 15) && (arrayOfBoolean[((j * 16 + (i1 + 1)) * 8 + i2)] != 0)) || ((i1 > 0) && (arrayOfBoolean[((j * 16 + (i1 - 1)) * 8 + i2)] != 0)) || ((i2 < 7) && (arrayOfBoolean[((j * 16 + i1) * 8 + (i2 + 1))] != 0)) || ((i2 > 0) && (arrayOfBoolean[((j * 16 + i1) * 8 + (i2 - 1))] != 0))) ? 1 : 0;
+                    int i3 = (arrayOfBoolean[((j * 16 + i1) * 8 + i2)] == false) && (((j < 15) && (arrayOfBoolean[(((j + 1) * 16 + i1) * 8 + i2)] != false)) || ((j > 0) && (arrayOfBoolean[(((j - 1) * 16 + i1) * 8 + i2)] != false)) || ((i1 < 15) && (arrayOfBoolean[((j * 16 + (i1 + 1)) * 8 + i2)] != false)) || ((i1 > 0) && (arrayOfBoolean[((j * 16 + (i1 - 1)) * 8 + i2)] != false)) || ((i2 < 7) && (arrayOfBoolean[((j * 16 + i1) * 8 + (i2 + 1))] != false)) || ((i2 > 0) && (arrayOfBoolean[((j * 16 + i1) * 8 + (i2 - 1))] != false))) ? 1 : 0;
 
                     if (i3 != 0) {
                         Material localMaterial = paramWorld.getMaterial(paramInt1 + j, paramInt2 + i2, paramInt3 + i1);
@@ -68,7 +70,7 @@ public class WorldGenLakes extends WorldGenerator {
         for (j = 0; j < 16; j++) {
             for (i1 = 0; i1 < 16; i1++) {
                 for (i2 = 0; i2 < 8; i2++) {
-                    if (arrayOfBoolean[((j * 16 + i1) * 8 + i2)] != 0) {
+                    if (arrayOfBoolean[((j * 16 + i1) * 8 + i2)] != false) {
                         paramWorld.setRawTypeId(paramInt1 + j, paramInt2 + i2, paramInt3 + i1, i2 >= 4 ? 0 : this.a);
                     }
                 }
@@ -78,7 +80,7 @@ public class WorldGenLakes extends WorldGenerator {
         for (j = 0; j < 16; j++) {
             for (i1 = 0; i1 < 16; i1++) {
                 for (i2 = 4; i2 < 8; i2++) {
-                    if ((arrayOfBoolean[((j * 16 + i1) * 8 + i2)] == 0) || (paramWorld.getTypeId(paramInt1 + j, paramInt2 + i2 - 1, paramInt3 + i1) != Block.DIRT.id) || (paramWorld.a(EnumSkyBlock.SKY, paramInt1 + j, paramInt2 + i2, paramInt3 + i1) <= 0))
+                    if ((arrayOfBoolean[((j * 16 + i1) * 8 + i2)] == false) || (paramWorld.getTypeId(paramInt1 + j, paramInt2 + i2 - 1, paramInt3 + i1) != Block.DIRT.id) || (paramWorld.a(EnumSkyBlock.SKY, paramInt1 + j, paramInt2 + i2, paramInt3 + i1) <= 0))
                         continue;
                     BiomeBase localBiomeBase = paramWorld.getWorldChunkManager().getBiome(paramInt1 + j, paramInt3 + i1);
                     if (localBiomeBase.t == Block.MYCEL.id)
@@ -95,7 +97,7 @@ public class WorldGenLakes extends WorldGenerator {
             for (j = 0; j < 16; j++) {
                 for (i1 = 0; i1 < 16; i1++) {
                     for (i2 = 0; i2 < 8; i2++) {
-                        int i4 = (arrayOfBoolean[((j * 16 + i1) * 8 + i2)] == 0) && (((j < 15) && (arrayOfBoolean[(((j + 1) * 16 + i1) * 8 + i2)] != 0)) || ((j > 0) && (arrayOfBoolean[(((j - 1) * 16 + i1) * 8 + i2)] != 0)) || ((i1 < 15) && (arrayOfBoolean[((j * 16 + (i1 + 1)) * 8 + i2)] != 0)) || ((i1 > 0) && (arrayOfBoolean[((j * 16 + (i1 - 1)) * 8 + i2)] != 0)) || ((i2 < 7) && (arrayOfBoolean[((j * 16 + i1) * 8 + (i2 + 1))] != 0)) || ((i2 > 0) && (arrayOfBoolean[((j * 16 + i1) * 8 + (i2 - 1))] != 0))) ? 1 : 0;
+                        int i4 = (arrayOfBoolean[((j * 16 + i1) * 8 + i2)] == false) && (((j < 15) && (arrayOfBoolean[(((j + 1) * 16 + i1) * 8 + i2)] != false)) || ((j > 0) && (arrayOfBoolean[(((j - 1) * 16 + i1) * 8 + i2)] != false)) || ((i1 < 15) && (arrayOfBoolean[((j * 16 + (i1 + 1)) * 8 + i2)] != false)) || ((i1 > 0) && (arrayOfBoolean[((j * 16 + (i1 - 1)) * 8 + i2)] != false)) || ((i2 < 7) && (arrayOfBoolean[((j * 16 + i1) * 8 + (i2 + 1))] != false)) || ((i2 > 0) && (arrayOfBoolean[((j * 16 + i1) * 8 + (i2 - 1))] != false))) ? 1 : 0;
 
                         if ((i4 == 0) || ((i2 >= 4) && (paramRandom.nextInt(2) == 0)) || (!paramWorld.getMaterial(paramInt1 + j, paramInt2 + i2, paramInt3 + i1).isBuildable()))
                             continue;
