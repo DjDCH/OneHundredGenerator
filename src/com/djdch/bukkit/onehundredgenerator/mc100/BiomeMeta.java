fd87c2315ed919d0a57c93e9689bc0dc3c5fad38
DjDCH <bukkit.coding@djdch.com>
2011-12-28 22:22:02 -0500

    Extended BiomeMeta from net.minecraft.server.BiomeMeta.

--- src/com/djdch/bukkit/onehundredgenerator/mc100/BiomeMeta.java
+++ src/com/djdch/bukkit/onehundredgenerator/mc100/BiomeMeta.java
@@ -1,18 +1,8 @@
 package com.djdch.bukkit.onehundredgenerator.mc100;
 
-import net.minecraft.server.WeightedRandomChoice;
-
-public class BiomeMeta extends WeightedRandomChoice {
-    @SuppressWarnings("rawtypes")
-    public Class a;
-    public int b;
-    public int c;
-
+public class BiomeMeta extends net.minecraft.server.BiomeMeta {
     @SuppressWarnings("rawtypes")
     public BiomeMeta(Class paramClass, int paramInt1, int paramInt2, int paramInt3) {
-        super(paramInt1);
-        this.a = paramClass;
-        this.b = paramInt2;
-        this.c = paramInt3;
+        super(paramClass, paramInt1, paramInt3, paramInt3);
     }
 }
